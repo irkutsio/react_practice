05b43e2d0c6ff3ade7677cd83e49c862a62b1dd1
Iryna Kutsevol <117863253+irkutsio@users.noreply.github.com>
2023-05-23 23:38:27 +0300

    practice

---
+++ src/components/header.jsx
@@ -0,0 +1,7 @@
+export const Header = ({className, id}) => {
+    return (
+      <p className={className} id={id}>
+        I am header
+      </p>
+    );
+  };
--- src/index.js
+++ src/index.js
@@ -2,9 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { Card } from 'components/card';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.querySelector('#root')).render(<Card />);
+
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// const paragrph = React.createElement('p', {
+//   id: 'p-id',
+//   children: ['I am p']
+// })
+
+// const paragraph = <p id="new-p">I am new P</p>;
+
+// const div = React.createElement('div', {
+//   name: 'main',
+//   className: 'test-class',
+//   id: 'test-id',
+//   children: [paragrph]
+// });
+
+// const div = (
+//   <div id="div-id" className="test-class">
+//     {paragraph}
+//   </div>
+// );
+
+// console.dir(div);
+
+
+
